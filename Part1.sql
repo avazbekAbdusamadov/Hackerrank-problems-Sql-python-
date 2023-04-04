f040f3fa04e83f43e19a52132d0a2db70c1e4e34
Avazbek Abdusamadov <111243372+avazbekAbdusamadov@users.noreply.github.com>
2023-04-04 22:09:11 +0500

    Update Part1.sql

--- Part1.sql
+++ Part1.sql
@@ -6,3 +6,15 @@ begin
    set @var=@var-1;
 end;
 
+/*   Query an alphabetically ordered list of all names in OCCUPATIONS, immediately 
+followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). 
+For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. 
+Sort the occurrences in ascending order, and output them in the following format:*/
+
+(select concat(name,'(',left(occupation,1),')') from occupations 
+ order by name asc)
+ union
+(select concat('There are a total of ',count(occupation),' ',lower(occupation) ) from occupations 
+group by occupation
+order by count(occupation), occupation);
